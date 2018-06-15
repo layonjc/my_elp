37589b7a5a98eb60903d60e670a6446714ee7b88
Raghu Betina <raghu@firstdraft.com>
2018-06-15 04:02:08 +0000

    Whitelist columns in Pic for ActiveAdmin

--- app/admin/pics.rb
+++ app/admin/pics.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Pic do
+
+ permit_params :caption, :image, :user_id, :restaurant_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
