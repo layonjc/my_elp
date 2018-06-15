7e9f980f5959fa5576b1fb9716f25539c1443b0a
Raghu Betina <raghu@firstdraft.com>
2018-06-15 04:02:13 +0000

    Only restaurant user can access show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :current_user_must_be_restaurant_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_restaurant_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_restaurant_user
     restaurant = Restaurant.find(params[:id])
