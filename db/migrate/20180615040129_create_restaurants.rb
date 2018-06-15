3dde6305869bbfc5a3bdb9668cbfb0e3f11f869e
Raghu Betina <raghu@firstdraft.com>
2018-06-15 04:01:29 +0000

    Generate restaurants resource with starter:resource

---
+++ db/migrate/20180615040129_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.integer :user_id
+
+      t.timestamps
+
+    end
+  end
+end
