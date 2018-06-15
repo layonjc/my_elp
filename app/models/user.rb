355e99ce09e846ebf6e46dae4cfae6bf9eb4dd02
Raghu Betina <raghu@firstdraft.com>
2018-06-15 04:01:46 +0000

    Direct association between users and reminders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :reminders,
+             :dependent => :destroy
+
   has_many   :restaurants,
              :dependent => :destroy
 
