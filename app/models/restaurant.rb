a966cf33de944e0946196723dcab87d74887ba2b
Raghu Betina <raghu@firstdraft.com>
2018-06-15 04:01:46 +0000

    Direct association between restaurants and reminders

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :reminders,
+             :dependent => :destroy
+
   has_many   :pics,
              :dependent => :destroy
 
