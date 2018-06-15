987d5b3d321070b4f9af0c3edd1678f41d4d7358
Raghu Betina <raghu@firstdraft.com>
2018-06-15 04:01:34 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "restaurants#index"
   # Routes for the Reminder resource:
   # CREATE
   get "/reminders/new", :controller => "reminders", :action => "new"
